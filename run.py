10259f38c2c6cab27b5b7eb859810df5a13fd99a
zhaoyangli-1 <zhl165@ucsd.edu>
2024-06-06 16:44:14 -0700

    add reverse run.py~

--- run.py
+++ run.py
@@ -933,24 +933,24 @@ def main(args):
             truncation=True,
         )
         # multiple references
-        if type(targets[0]) is not list:
-            # Setup the tokenizer for targets
-            with tokenizer.as_target_tokenizer():
-                labels = tokenizer(
-                    targets, 
-                    max_length=max_target_length, 
-                    padding=padding, 
-                    truncation=True,
-                )
+        # if type(targets[0]) is not list:
+        # Setup the tokenizer for targets
+        with tokenizer.as_target_tokenizer():
+            labels = tokenizer(
+                targets, 
+                max_length=max_target_length, 
+                padding=padding, 
+                truncation=True,
+            )
 
-            # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
-            # padding in the loss.
-            if padding == "max_length" and args.ignore_pad_token_for_loss:
-                labels["input_ids"] = [
-                    [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
-                ]
+        # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
+        # padding in the loss.
+        if padding == "max_length" and args.ignore_pad_token_for_loss:
+            labels["input_ids"] = [
+                [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+            ]
 
-            model_inputs["labels"] = labels["input_ids"]
+        model_inputs["labels"] = labels["input_ids"]
         return model_inputs
 
     refs = [sample["ref"] for sample in raw_datasets["valid"]["data"]]
