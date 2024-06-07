b6156669c233de4512824ba6bf333fb7063f9116
zhaoyangli-1 <zhl165@ucsd.edu>
2024-06-06 17:06:04 -0700

    fix bug

--- data/reverse_webnlg.py
+++ data/reverse_webnlg.py
@@ -1,10 +1,11 @@
 import json
 import numpy as np
 import os
+import random
 
 CUURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-def exchange_input_ref(input_file_path, out_put_file):
+def exchange_input_ref_train(input_file_path, out_put_file):
     dataset = []
     with open(input_file_path, "r") as f:
         for line in f:
@@ -20,16 +21,35 @@ def exchange_input_ref(input_file_path, out_put_file):
                 f.write("\n")
             f.write(json.dumps(line))
     print(f"File saved in {out_put_file}.")
+
+
+def exchange_input_val_test(input_file_path, out_put_file):
+    dataset = []
+    with open(input_file_path, "r") as f:
+        for line in f:
+            line = json.loads(line)
+            line_input= line["input"]
+            line["input"] = random.choice(line["ref"])
+            line["ref"] = [line_input]    
+            dataset.append(line)
     
+    with open(out_put_file, "w") as f:
+        for i, line in enumerate(dataset):
+            if i > 0:
+                f.write("\n")
+            f.write(json.dumps(line))
+    print(f"File saved in {out_put_file}.")
+
+
 def main():
     save_folder = os.path.join(CUURRENT_FOLDER, "reverse-webnlg")
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
-    exchange_input_ref(os.path.join(CUURRENT_FOLDER, "webnlg/train.json"), os.path.join(save_folder, "train.json"))
-    exchange_input_ref(os.path.join(CUURRENT_FOLDER, "webnlg/val.json"), os.path.join(save_folder, "val.json"))
-    exchange_input_ref(os.path.join(CUURRENT_FOLDER, "webnlg/test_unseen.json"), os.path.join(save_folder, "test_unseen.json"))
-    exchange_input_ref(os.path.join(CUURRENT_FOLDER, "webnlg/test_both.json"), os.path.join(save_folder, "test_both.json"))
-    exchange_input_ref(os.path.join(CUURRENT_FOLDER, "webnlg/test_seen.json"), os.path.join(save_folder, "test_seen.json"))
+    exchange_input_ref_train(os.path.join(CUURRENT_FOLDER, "webnlg/train.json"), os.path.join(save_folder, "train.json"))
+    exchange_input_val_test(os.path.join(CUURRENT_FOLDER, "webnlg/val.json"), os.path.join(save_folder, "val.json"))
+    exchange_input_val_test(os.path.join(CUURRENT_FOLDER, "webnlg/test_unseen.json"), os.path.join(save_folder, "test_unseen.json"))
+    exchange_input_val_test(os.path.join(CUURRENT_FOLDER, "webnlg/test_both.json"), os.path.join(save_folder, "test_both.json"))
+    exchange_input_val_test(os.path.join(CUURRENT_FOLDER, "webnlg/test_seen.json"), os.path.join(save_folder, "test_seen.json"))
     print("Done")
    
 
--- run.py
+++ run.py
@@ -886,45 +886,10 @@ def main(args):
     max_target_length = args.max_target_length
     padding = "max_length" if args.pad_to_max_length else False
 
-    # def preprocess_function(examples):
-    #     inputs = examples[args.input_column]
-    #     targets = examples[args.target_column]
-    #     # breakpoint()
-    #     inputs = [prefix + inp for inp in inputs]
-    #     model_inputs = tokenizer(
-    #         inputs, 
-    #         max_length=args.max_source_length, 
-    #         padding=padding, 
-    #         truncation=True,
-    #     )
-    #     # multiple references
-    #     if type(targets[0]) is not list:
-    #         # Setup the tokenizer for targets
-    #         with tokenizer.as_target_tokenizer():
-    #             labels = tokenizer(
-    #                 targets, 
-    #                 max_length=max_target_length, 
-    #                 padding=padding, 
-    #                 truncation=True,
-    #             )
-
-    #         # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
-    #         # padding in the loss.
-    #         if padding == "max_length" and args.ignore_pad_token_for_loss:
-    #             labels["input_ids"] = [
-    #                 [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
-    #             ]
-
-    #         model_inputs["labels"] = labels["input_ids"]
-    #     return model_inputs
-    
     def preprocess_function(examples):
         inputs = examples[args.input_column]
         targets = examples[args.target_column]
-        if isinstance(inputs[0], list):
-            inputs = [prefix.join(inp) for inp in inputs]
-        else:
-            inputs = [prefix + inp for inp in inputs]
+        # breakpoint()
         inputs = [prefix + inp for inp in inputs]
         model_inputs = tokenizer(
             inputs, 
@@ -933,25 +898,60 @@ def main(args):
             truncation=True,
         )
         # multiple references
-        # if type(targets[0]) is not list:
-        # Setup the tokenizer for targets
-        with tokenizer.as_target_tokenizer():
-            labels = tokenizer(
-                targets, 
-                max_length=max_target_length, 
-                padding=padding, 
-                truncation=True,
-            )
+        if type(targets[0]) is not list:
+            # Setup the tokenizer for targets
+            with tokenizer.as_target_tokenizer():
+                labels = tokenizer(
+                    targets, 
+                    max_length=max_target_length, 
+                    padding=padding, 
+                    truncation=True,
+                )
 
-        # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
-        # padding in the loss.
-        if padding == "max_length" and args.ignore_pad_token_for_loss:
-            labels["input_ids"] = [
-                [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
-            ]
+            # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
+            # padding in the loss.
+            if padding == "max_length" and args.ignore_pad_token_for_loss:
+                labels["input_ids"] = [
+                    [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+                ]
 
-        model_inputs["labels"] = labels["input_ids"]
+            model_inputs["labels"] = labels["input_ids"]
         return model_inputs
+    
+    # def preprocess_function(examples):
+    #     inputs = examples[args.input_column]
+    #     targets = examples[args.target_column]
+    #     if isinstance(inputs[0], list):
+    #         inputs = [prefix.join(inp) for inp in inputs]
+    #     else:
+    #         inputs = [prefix + inp for inp in inputs]
+    #     inputs = [prefix + inp for inp in inputs]
+    #     model_inputs = tokenizer(
+    #         inputs, 
+    #         max_length=args.max_source_length, 
+    #         padding=padding, 
+    #         truncation=True,
+    #     )
+    #     # multiple references
+    #     # if type(targets[0]) is not list:
+    #     # Setup the tokenizer for targets
+    #     with tokenizer.as_target_tokenizer():
+    #         labels = tokenizer(
+    #             targets, 
+    #             max_length=max_target_length, 
+    #             padding=padding, 
+    #             truncation=True,
+    #         )
+
+    #     # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
+    #     # padding in the loss.
+    #     if padding == "max_length" and args.ignore_pad_token_for_loss:
+    #         labels["input_ids"] = [
+    #             [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+    #         ]
+
+    #     model_inputs["labels"] = labels["input_ids"]
+    #     return model_inputs
 
     refs = [sample["ref"] for sample in raw_datasets["valid"]["data"]]
     # process for sacrebleu
